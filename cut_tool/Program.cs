aec6aa367afc522de9a1262a5514994581caa922
KevJSDevelopment <Kshhn12@gmail.com>
2024-07-21 11:21:41 -0400

    working through capturing the field value

---
+++ cut_tool/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string delimiter = ",";
+        List<int> fields = new List<int>();
+        string file = null;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i]){
+                case "-f":
+                    if (i + 1 < args.Length)
+                    {
+                        fields.AddRange(args[++i].Split(',').Select(int.Parse));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: -f option requires a value");
+                        return;
+                    }
+                    break;
+                case "-d":
+                    if(i + 1 < args.Length)
+                    {
+                        delimiter = args[i++];
+                    }
+                    else {
+                        delimiter = "\t";
+                    }
+                    break;
+                default:
+                    if(file == null)
+                    {
+                        file = args[i];
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"Error: Unexpected argument {args[i]}");
+                        return;
+                    }
+                    break;
+            }
+        }
+
+        if(file == null) {
+            Console.WriteLine("Usage: cut_tool [-f][fields] [-d][deliminator] <file-path>");
+            return;
+        }
+
+        var lines = File.ReadAllLines(file);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int tabCount = 0;
+            int delLength = delimiter.Length;
+            string fieldValue = "";
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                if(lines[i].Substring(j, delLength) == delimiter){
+                    tabCount++;
+                }
+
+                // if(fields.)
+            }
+        }
+    }
+}
